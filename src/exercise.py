1c623230d5920600c73c6bb3392f70991346f091
cabosh <callum.bosher@gmail.com>
2020-05-15 15:39:04 +0100

    Attempted commit

--- src/exercise.py
+++ src/exercise.py
@@ -1,5 +1,12 @@
 def main():
     #write your code below this line
+    num1 = int(input("Enter your first number: "))
+    num2 = int(input("Enter your second number: "))
+    num3 = int(input("Enter your third number: "))
+
+    total = num1 + num2 + num3
+
+    print("The average is " + str(total / 3))
 
 if __name__ == '__main__':
     main()
